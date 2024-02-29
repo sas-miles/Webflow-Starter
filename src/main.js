882f037791062327b517516202c21b336c9b4955
sas-miles <132398659+sas-miles@users.noreply.github.com>
2024-02-29 10:54:00 -0500

    added placeholder script in main.js

--- src/main.js
+++ src/main.js
@@ -1,4 +1,25 @@
+import barba from '@barba/core';
+import { restartWebflow } from "@finsweet/ts-utils";
+
+console.log(restartWebflow)
 window.Webflow ||= [];
 window.Webflow.push(async () => {
-  console.log('Webflow loaded');
+  
+  barba.init({
+    transitions: [{
+      name: 'default-transition',
+      leave() {
+        console.log('leave');
+      },
+      enter() {
+        console.log('enter');
+      }
+    }]
+  });
+
+})
+
+barba.hooks.afterEnter((data) => {
+  console.log('afterEnter');
+  restartWebflow();
 })
